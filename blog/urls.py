363d702d3a56931f9bec5362ac8ce1207999db30
jtlsan <jtlsan@naver.com>
2019-01-11 19:38:49 +0900

    my first html

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
--- proj_1/urls.py
+++ proj_1/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, include
 def mysum(reuqest, x, y):
     result = x+y
     return HttpResponse("result = {}".format(result))
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mysum/<int:x>/<int:y>/', mysum),
+    path('', include('blog.urls')),
 ]
